4a19011327102c540e4e9f397d20e37ed54bf130
Joshua Knight <j.knight@kainos.com>
2019-11-24 12:51:26 +0000

    Adding email and password fields to signup view

--- AssistedHome/SignUpViewController.swift
+++ AssistedHome/SignUpViewController.swift
@@ -16,13 +16,37 @@ class SignUpViewController: UIViewController {
         
     }
     @IBOutlet weak var FooterView: UIView!
+    @IBOutlet weak var SignUpEmailField: UITextField!
+    @IBOutlet weak var SignUpPasswordField: UITextField!
+    @IBOutlet weak var SignUpConfirmPasswordField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Set background gradient and footer
         view.setGradientBackground(colorOne: Colours.lightBlue, colorTwo: Colours.purple)
-        
         FooterView.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+        
+        // Call extension to set text field style
+        SignUpEmailField.underlined()
+        SignUpPasswordField.underlined()
+        SignUpConfirmPasswordField.underlined()
+        
+        SignUpPasswordField.isSecureTextEntry               = true
+        SignUpConfirmPasswordField.isSecureTextEntry        = true
+        SignUpPasswordField.attributedPlaceholder           = NSAttributedString(string: "Password",
+                                                              attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
+        SignUpConfirmPasswordField.attributedPlaceholder    = NSAttributedString(string: "Confirm Password",
+                                                              attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
+        SignUpEmailField.attributedPlaceholder              = NSAttributedString(string: "assistedhome@example.com",
+                                                              attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
+   
+        
+        
+        
+        
+
+        
     }
     
     
