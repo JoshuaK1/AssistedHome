f055aca392a218f62d269b3721b4ecdffd70b3f2
Joshua Knight <j.knight@kainos.com>
2019-11-24 09:36:24 +0000

    Initial Commit

---
+++ AssistedHomeTests/AssistedHomeTests.swift
@@ -0,0 +1,34 @@
+//
+//  AssistedHomeTests.swift
+//  AssistedHomeTests
+//
+//  Created by Joshua Knight on 24/11/2019.
+//  Copyright © 2019 Joshua Knight. All rights reserved.
+//
+
+import XCTest
+@testable import AssistedHome
+
+class AssistedHomeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
