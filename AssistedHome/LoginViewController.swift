f97f02effac1da5b9f8323dc272f6a967f728933
Joshua Knight <j.knight@kainos.com>
2019-11-25 22:44:16 +0000

    Adding text fields and button to loginViewController

--- AssistedHome/LoginViewController.swift
+++ AssistedHome/LoginViewController.swift
@@ -7,11 +7,16 @@
 //
 
 import Foundation
+import Firebase
 import UIKit
 
 class LoginViewController: UIViewController {
     
+    @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var FooterView: UIView!
+    @IBOutlet weak var loginEmailField: UITextField!
+    @IBOutlet weak var loginPasswordField: UITextField!
+    @IBOutlet weak var loginConfirmPasswordField: UITextField!
     
     @IBAction func signUpButton(_ sender: Any) {
         performSegue(withIdentifier: "showLoginToSignUp", sender: self)
@@ -23,6 +28,25 @@ class LoginViewController: UIViewController {
         FooterView.backgroundColor = UIColor.white.withAlphaComponent(0.5)
         
         view.setGradientBackground(colorOne: Colours.lightBlue, colorTwo: Colours.purple)
+        
+        // Function to underline text fields
+        loginEmailField.underlined()
+        loginPasswordField.underlined()
+        loginConfirmPasswordField.underlined()
+        
+        loginPasswordField.isSecureTextEntry               = true
+        loginConfirmPasswordField.isSecureTextEntry        = true
+        loginPasswordField.attributedPlaceholder           = NSAttributedString(string: "Password",
+                                                                                 attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
+        loginConfirmPasswordField.attributedPlaceholder    = NSAttributedString(string: "Confirm Password",
+                                                                                 attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
+        loginEmailField.attributedPlaceholder              = NSAttributedString(string: "assistedhome@example.com",
+                                                                                 attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
+        
+        
+        loginButton.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+        
+        loginButton.layer.cornerRadius = 7
     }
     
     
