6e35129624043a51a4c10cd475f768a5205cce32
Joshua Knight <j.knight@kainos.com>
2019-11-27 14:41:52 +0000

    Adding uitext view and styling to announceViewController

--- AssistedHome/AnnounceViewController.swift
+++ AssistedHome/AnnounceViewController.swift
@@ -12,7 +12,8 @@ import UIKit
 class AnnounceViewController: UIViewController {
     
     @IBOutlet weak var textView: UIView!
-
+    @IBOutlet weak var announceTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -20,6 +21,9 @@ class AnnounceViewController: UIViewController {
         
         textView.backgroundColor = UIColor.white.withAlphaComponent(0.5)
         textView.roundCornerView(cornerRadius: 8.0)
+        
+        announceTextView.font = UIFont(name: "Arial", size: 20)
+        announceTextView.textColor = UIColor.white
         
     }
     
