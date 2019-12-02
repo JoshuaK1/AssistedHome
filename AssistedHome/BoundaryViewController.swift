d7e5608c547e412800ffe0ca359f342a28b76e6f
Joshua Knight <j.knight@kainos.com>
2019-12-02 10:36:51 +0000

    Opening map on current location

--- AssistedHome/BoundaryViewController.swift
+++ AssistedHome/BoundaryViewController.swift
@@ -9,18 +9,50 @@
 import Foundation
 import UIKit
 import MapKit
+import CoreLocation
 
-class BoundaryViewController: UIViewController, MKMapViewDelegate {
+class BoundaryViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    
     @IBOutlet weak var mapView: MKMapView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let noLocation = CLLocationCoordinate2D()
+        let viewRegion = MKCoordinateRegion.init(center: noLocation, latitudinalMeters: 200, longitudinalMeters: 200)
+        mapView.setRegion(viewRegion, animated: false)
+        
+        let locationManager             = CLLocationManager()
+        locationManager.delegate        = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        // Check if Location Services are enabled
+        if (CLLocationManager.locationServicesEnabled()){
+            locationManager.requestWhenInUseAuthorization()
+            locationManager.requestAlwaysAuthorization()
+        }
+        
+        // Zoom to current user location
+        if let userLocation = locationManager.location?.coordinate {
+            let viewRegion = MKCoordinateRegion.init(center: userLocation, latitudinalMeters: 200, longitudinalMeters: 200)
+            mapView.setRegion(viewRegion, animated: false)
+        }
+        
+        DispatchQueue.main.async {
+            locationManager.startUpdatingLocation()
+        }
+        
         view.setGradientBackground(colorOne: Colours.lightBlue, colorTwo: Colours.purple)
         
         mapView.layer.cornerRadius = 8.0
         
+        // show current user location
+        locationManager.requestWhenInUseAuthorization()
+        
+        mapView.mapType           = .standard
+        mapView.showsUserLocation = true
+        mapView.showsScale        = true
         
     }
     
