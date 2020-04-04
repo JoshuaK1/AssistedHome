3673f41123136c2405a54ca28923c8834fa0074f
Joshua Knight <j.knight@kainos.com>
2020-04-04 15:35:14 +0100

    Checkboxes being animated

--- AssistedHome/PopupViewController.swift
+++ AssistedHome/PopupViewController.swift
@@ -8,11 +8,38 @@
 
 import Foundation
 import UIKit
+import BEMCheckBox
 
 class PopupViewController: UIViewController {
     
+    @IBOutlet weak var checkbox: BEMCheckBox!
+    @IBOutlet weak var popupView: UIView!
+    
+    func animateSafeCheckMark(){
+        // Wait 1 second before animating check mark
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            self.checkbox.setOn(true, animated: true)
+            self.dismissView(dismiss: true)
+        }
+    }
+    
+    func dismissView(dismiss: Bool) -> Void {
+        if dismiss {
+            // Wait 1 second before dismissing the view
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                self.dismiss(animated: true, completion: nil)
+            }
+        }
+    }
+    
     override func viewDidLoad() {
-        //popupView.layer.cornerRadius = 10.0
+        popupView.layer.cornerRadius = 10.0
+        self.checkbox.on = false
+        
+        // Call fucntion to animate check mark
+        animateSafeCheckMark()
+        
+        
     }
     
 }
