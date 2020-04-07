e925555fd9427808c2fe2ac132b65e04ced41583
Joshua Knight <j.knight@kainos.com>
2020-04-07 16:11:42 +0100

    Adding UserAccountViewController

---
+++ AssistedHome/UserAccountViewController.swift
@@ -0,0 +1,15 @@
+//
+//  UserAccountViewController.swift
+//  AssistedHome
+//
+//  Created by Joshua Knight on 07/04/2020.
+//  Copyright © 2020 Joshua Knight. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class UserAccountViewController: UIViewController {
+    
+    
+}
