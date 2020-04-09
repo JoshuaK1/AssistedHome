2992952286a62ef930fa8f14793b3eba69208b4e
Joshua Knight <j.knight@kainos.com>
2020-04-09 15:01:24 +0100

    Stored alerts can now be sent to Alexa

--- AssistedHome/DetailedStoredAlertsViewController.swift
+++ AssistedHome/DetailedStoredAlertsViewController.swift
@@ -10,12 +10,54 @@ import UIKit
 
 class DetailedStoredAlertsViewController: UIViewController {
     
+    var completeStringArray = [String]()
+    
     @IBOutlet weak var alertTextView: UITextView!
     
     @IBOutlet weak var sendAlertButton: UIButton!
     
+    func sendNotifications(message: String){
+        let url = URL(string: "https://api.notifymyecho.com/v1/NotifyMe?notification=\(message))&accessCode=amzn1.ask.account.AFYWLPJBB37FH5BSRVPBQCYDJLWH6KDO2GEC22AOUX5AF7INTD2OKVNVWW2DWJ6FEPWVAPJQDT5QUQFF3J64LV5TE66EIH7LMON6F5U6DGGEUDD6NHXRWZDLIRIPO3QM3ROFPJ662LEURCVA5XO7EBWYUZNYF7XSBLHXGZB6JBE3UVPMRNMSTK55XKCY74GKDBHTEZEA64TENUY")!
+        
+        let session = URLSession.shared
+        
+        let request = NSMutableURLRequest(url: url)
+        
+        request.httpMethod = "POST"
+        
+        let task = session.dataTask(with: request as URLRequest, completionHandler: { data, response, error in
+            
+            guard error == nil else {
+                return
+            }
+            
+            guard data != nil else {
+                return
+            }
+        })
+        
+        task.resume()
+    }
+    
     @IBAction func sendAlertButton(_ sender: Any) {
         performSegue(withIdentifier: "detailedAlertConfirmation", sender: self)
+        
+        let message = alertTextView.text!
+        
+        let receivedText = alertTextView.text!
+        
+        let stringArray = receivedText.components(separatedBy: " ")
+        
+        for string in stringArray {
+            let string = string + "%20"
+            completeStringArray.append(string)
+        }
+        
+        let joinedString = completeStringArray.joined()
+        
+        print(joinedString)
+        
+        sendNotifications(message: joinedString)
     }
     
     //Add support for swipe gestures
@@ -48,6 +90,8 @@ class DetailedStoredAlertsViewController: UIViewController {
         print("Alert taken from struct", alert)
         
     }
+    
+
 
     override func viewDidLoad() {
         
